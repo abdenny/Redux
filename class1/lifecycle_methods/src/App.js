342f0543d835fab689a5c4e8c66d97d1c01d0a4f
Austin Denny <austinbdenny@gmail.com>
2020-04-06 16:37:29 -0500

    End of class.

--- class1/lifecycle_methods/src/App.js
+++ class1/lifecycle_methods/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    console.log('COmponent will mount');
+  }
+  componentDidMount() {
+    console.log('COmponent did mount');
+  }
+
+  render() {
+    console.log('Rendered COmponent');
+    return <>Lifecycle methods</>;
+  }
 }
 
 export default App;
