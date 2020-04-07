904659360f56a74d5aac9ed7dc3a25a050a665f7
Austin Denny <austinbdenny@gmail.com>
2020-04-07 13:56:33 -0500

    Layed out index.

--- class2/shoppingcart/src/index.js
+++ class2/shoppingcart/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path='' component={} />
+          <Route exact path='' component={} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
