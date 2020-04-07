5e37a93fbabbb778f13577652ad52447da92e5ee
Austin Denny <austinbdenny@gmail.com>
2020-04-07 09:55:48 -0500

    Scaffold out.

--- class1/lifecycle_methods/src/App.js
+++ class1/lifecycle_methods/src/App.js
@@ -1,20 +1,56 @@
-import React from 'react';
-
-class App extends React.Component {
+import React, { Component } from 'react';
+import Child from './Child';
+class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+      isLoaded: false,
+      items: [],
+    };
+    console.log("i'm a constructor");
   }
   componentWillMount() {
-    console.log('COmponent will mount');
+    console.log('Component will mount');
   }
   componentDidMount() {
-    console.log('COmponent did mount');
+    console.log('component did mount');
+    // fetch("https://corona.lmao.ninja/states")
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     this.setState({
+    //       isLoaded: true,
+    //       items: json
+    //     });
+    //   });
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      });
+    }, 3000);
   }
-
   render() {
-    console.log('Rendered COmponent');
-    return <>Lifecycle methods</>;
+    console.log('rendered component');
+    if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+          {/* Lifecycle Methods <br />
+        <Child test={4} /> */}
+          Loaded Data
+          <ul>
+            {this.state.items.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.state} | {item.cases}
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      );
+    }
   }
 }
-
 export default App;
